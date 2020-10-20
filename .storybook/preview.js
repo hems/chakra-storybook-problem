6558e36910f62c85a1be46a4c6427f48e9a5b79f
hems <hems.inlet@gmail.com>
2020-10-20 10:12:11 +0100

    updating preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,22 +1,19 @@
-import CSSReset from "@chakra-ui/css-reset"
-import theme from "../src/chakra"
 import { ChakraProvider, Box } from "@chakra-ui/core"
 import { addDecorator } from "@storybook/react"
 import * as React from "react"
+import { extendTheme } from '@chakra-ui/core';
 
-const newTheme = {
-  ...theme,
+const customTheme = extendTheme({
   config: {
-    initialColorMode: "dark",
-    useSystemColorMode: false,
+    initialColorMode: 'dark',
+    useSystemColorMode: false
   }
-}
+});
+
 
 export const Chakra = ({ children }) => {
-  console.log("theme.config ->", newTheme.config)
-  
   return (
-    <ChakraProvider CSSReset theme={newTheme} initialColorMode="dark">
+    <ChakraProvider theme={customTheme}>
       <Box p={5}>
         {children}
       </Box>
