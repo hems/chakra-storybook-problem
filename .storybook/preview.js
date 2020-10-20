a36c7a901d7cf63425effe5ad1967ee0ab5c389c
hems <hems.inlet@gmail.com>
2020-10-20 10:33:33 +0100

    fixed initial color mode by adding preview-head.html

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,26 +1,20 @@
-import { ChakraProvider, Box } from "@chakra-ui/core"
-import { addDecorator } from "@storybook/react"
-import * as React from "react"
-import { extendTheme } from '@chakra-ui/core';
-
-const customTheme = extendTheme({
-  config: {
-    initialColorMode: 'dark',
-    useSystemColorMode: false
-  }
-});
+import { ChakraProvider, Box } from "@chakra-ui/core";
+import { addDecorator } from "@storybook/react";
+import * as React from "react";
+import { extendTheme } from "@chakra-ui/core";
+import theme from '../src/chakra'
 
 
 export const Chakra = ({ children }) => {
   return (
-    <ChakraProvider theme={customTheme}>
-      <Box p={5}>
-        {children}
-      </Box>
+    <ChakraProvider theme={theme}>
+      {children}
     </ChakraProvider>
-  )
-}
+  );
+};
 
-addDecorator(StoryFn => <Chakra>
-  <StoryFn />
-</Chakra>)
+addDecorator((StoryFn) => (
+  <Chakra>
+    <StoryFn />
+  </Chakra>
+));
